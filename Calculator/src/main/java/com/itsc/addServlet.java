c6900a1f7fc195e03131aab19b789262aef4235c
Nebiyou-Daniel <118824653+Nebiyou-Daniel@users.noreply.github.com>
2023-11-15 06:19:57 -0800

    Add files via upload
    I have made an employee management system and a calculator projects. I am yet to fill them with code.

---
+++ Calculator/src/main/java/com/itsc/addServlet.java
@@ -0,0 +1,27 @@
+package com.itsc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class addServlet extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+		
+		PrintWriter pw = response.getWriter();
+		response.setContentType("text/html");
+		
+//		String url = "jdbc:mysql:///calculator";
+//		String username = "root";
+//		String password = "the1trueND!";
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+		doGet(request, response);
+	}
+}
